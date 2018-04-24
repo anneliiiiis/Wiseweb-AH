3b3ad346b0451a519b6ee2e8318d992cc3c56689
Anneliis Halling <anneliis.halling@wisemedia.ee>
2018-04-24 20:19:45 +0300

    Wrapper class added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Container } from "./Wrapper";
+import { Container, ColumnPaddingY } from "./Wrapper";
 import PhoneNumber from 'react-phone-number';
 import FontAwesome from 'react-fontawesome';
 
@@ -9,26 +9,26 @@ export default class Footer extends Component {
       <footer>
         <Container>
           <div className="row py-3">
-            <div className="col-md-3 py-2">
+            <ColumnPaddingY breakpoint="md" colNumber="3" paddingNumber="2">
               <FontAwesome name="copyright" className="pr-2"/>
               {this.props.copyright}
-            </div>
-            <div className="col-md-3 py-2">
+            </ColumnPaddingY>
+            <ColumnPaddingY breakpoint="md" colNumber="3" paddingNumber="2">
               <FontAwesome name="map-marker" className="pr-2"/>
               <a href={this.props.addressLink} target="_blank">
                 {this.props.address}
               </a>
-            </div>
-            <div className="col-md-3 py-2">
+            </ColumnPaddingY>
+            <ColumnPaddingY breakpoint="md" colNumber="3" paddingNumber="2">
               <FontAwesome name="envelope" className="pr-2"/>
               <a href={"mailto:" + this.props.email}>
                 {this.props.email}
               </a>
-            </div>
-            <div className="col-md-3 py-2">
+            </ColumnPaddingY>
+            <ColumnPaddingY breakpoint="md" colNumber="3" paddingNumber="2">
               <FontAwesome name="phone" className="pr-2"/>
               <PhoneNumber number={this.props.phone} isLinked={true}/>
-            </div>
+            </ColumnPaddingY>
           </div>
         </Container>
       </footer>
