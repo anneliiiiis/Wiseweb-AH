baafc529c18aa6c42d1ffbdf5e4d8af752c66e23
Anneliis Halling <anneliis.halling@wisemedia.ee>
2018-04-24 14:48:36 +0300

    Start

---
+++ src/views/Home.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { PrimaryButton } from "../components/Button";
+import { Container, Row } from "../components/Wrapper";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <header>
+        <Container>
+          <PrimaryButton >
+            Hello
+          </PrimaryButton>
+        </Container>
+      </header>
+    )
+  }
+}
